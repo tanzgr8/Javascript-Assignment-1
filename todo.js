1410cc2af2654cca9b39fb7b458a5cb7d6c5fab1
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-05-27 12:47:12 +0530

    Final Edit

--- todo.js
+++ todo.js
@@ -1,8 +1,7 @@
   const add= document.querySelector(".add");
   const input = document.querySelector(".newtodo"); 
   const todoList = document.querySelector(".todo");
-const check = document.querySelectorAll("input[name=checkbox");
-console.log(check);
+
   showTasks();
   add.onclick = ()=>{ 
     let Value=input.value;
@@ -30,10 +29,10 @@ function showTasks(){
   let newLiTag = "";
   listArray.forEach((element, index) => {
     if(element.status=="pending"){
-      newLiTag += `<li id="${index}"><input type="checkbox" id="checkbox${index}" onchange="change(${index})">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button><button class="ui inverted primary mini right floated button">EDIT</button></li >`;
+      newLiTag += `<li id="${index}"><input type="checkbox" id="checkbox${index}"  class="check" onchange="change(${index})">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button><button class="ui inverted primary mini right floated button" id="e${index}">EDIT</button></li >`;
     }
     else{
-      newLiTag += `<li id="${index}"><input type="checkbox" class="check" checked id="checkbox${index}">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button>`;
+      newLiTag += `<li id="${index}"><input type="checkbox" class="check" id="checkbox${index}">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button>`;
     }
  
   });
@@ -47,6 +46,13 @@ function deleteTask(index){
   showTasks(); //call the showTasks function
 }
 function change(id){
+  var checkbox = document.getElementById("checkbox"+id);
 var li = document.getElementById(id);
-li.classList.add("done");
+var edit = document.getElementById('e'+id);
+if(checkbox.checked){
+  edit.style.display="none";
+}else{
+  edit.style.display="inline-block";
+}
+li.classList.toggle("done");
 }
