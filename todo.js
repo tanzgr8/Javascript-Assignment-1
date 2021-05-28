c09fd5c7e4bb53c0c889bfea08a2e64815215d8d
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-05-28 11:24:28 +0530

    Final Edit

--- todo.js
+++ todo.js
@@ -1,58 +1,58 @@
-  const add= document.querySelector(".add");
-  const input = document.querySelector(".newtodo"); 
-  const todoList = document.querySelector(".todo");
+const add = document.querySelector(".add");
+const input = document.querySelector(".newtodo");
+const todoList = document.querySelector(".todo");
 
-  showTasks();
-  add.onclick = ()=>{ 
-    let Value=input.value;
+showTasks();
+add.onclick = () => {
+  let Value = input.value;
   let getLSData = localStorage.getItem("NewTodo"); //getting localstorage
-  if(getLSData == null){ //if localstorage has no data
+  if (getLSData == null) { //if localstorage has no data
     listArray = []; //create a blank array
-  }else{
+  } else {
     listArray = JSON.parse(getLSData);  //transforming json string into a js object
   }
-  let obj ={ 'task':Value,'status':'pending' } ;
+  let obj = { 'task': Value, 'status': 'pending' };
   listArray.push(obj) //pushing or adding new value in array
 
   localStorage.setItem("NewTodo", JSON.stringify(listArray)); //transforming js object into a json string
-  input.value="";
-  showTasks(); 
+  input.value = "";
+  showTasks();
 }
-function showTasks(){
+function showTasks() {
   let getLocalStorageData = localStorage.getItem("NewTodo");
 
-  if(getLocalStorageData == null){
+  if (getLocalStorageData == null) {
     listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
+  } else {
+    listArray = JSON.parse(getLocalStorageData);
   }
   let newLiTag = "";
   listArray.forEach((element, index) => {
-    if(element.status=="pending"){
+    if (element.status == "pending") {
       newLiTag += `<li id="${index}"><input type="checkbox" id="checkbox${index}"  class="check" onchange="change(${index})">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button><button class="ui inverted primary mini right floated button" id="e${index}">EDIT</button></li >`;
     }
-    else{
+    else {
       newLiTag += `<li id="${index}"><input type="checkbox" class="check" id="checkbox${index}">${element.task}<button class="ui inverted red mini right floated button" onClick="deleteTask(${index})">DELETE</button>`;
     }
- 
+
   });
   todoList.innerHTML = newLiTag;
 }
-function deleteTask(index){
+function deleteTask(index) {
   let getLSData = localStorage.getItem("NewTodo");
   listArray = JSON.parse(getLSData);
   listArray.splice(index, 1); //delete or remove the li
   localStorage.setItem("NewTodo", JSON.stringify(listArray));
   showTasks(); //call the showTasks function
 }
-function change(id){
-  var checkbox = document.getElementById("checkbox"+id);
-var li = document.getElementById(id);
-var edit = document.getElementById('e'+id);
-if(checkbox.checked){
-  edit.style.display="none";
-}else{
-  edit.style.display="inline-block";
-}
-li.classList.toggle("done");
+function change(id) {
+  var checkbox = document.getElementById("checkbox" + id);
+  var li = document.getElementById(id);
+  var edit = document.getElementById('e' + id);
+  if (checkbox.checked) {
+    edit.style.display = "none";
+  } else {
+    edit.style.display = "inline-block";
+  }
+  li.classList.toggle("done");
 }
